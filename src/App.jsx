d71083158a92fa0773d59eaf769bb5707ee1bef9
bonny-art <svitlana.otenko@gmail.com>
2025-05-08 01:05:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.module.css";
+import Container from "./components/Container/Container";
+import Section from "./components/Section/Section";
+
+function App() {
+  return (
+    <>
+      <Section>
+        <Container>
+          <p>Hello</p>
+        </Container>
+      </Section>
+    </>
+  );
+}
+
+export default App;
